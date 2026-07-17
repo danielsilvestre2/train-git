40d4ddda3fd79769f69d8beda14c52bfbfc71c8c
danielsilvestre2 <dlds55224@gmail.com>
2026-07-17 17:53:57 -0300

    feat: code with map,filter and find with a user array

--- script.js
+++ script.js
@@ -1,61 +1,79 @@
- const functions1 = (hello) => {
-  console.log('Hello, World!')
-  hello()
-  setTimeout(() => {
-    console.log('Hello Universe!')
-  }, 500)
-}
+//  const functions1 = (hello) => {
+//   console.log('Hello, World!')
+//   hello()
+//   setTimeout(() => {
+//     console.log('Hello Universe!')
+//   }, 500)
+// }
 
-const hello = () => {
-  setTimeout(() => {
-    console.log('Hello Galaxy!')
-  }, 1000)
-}
+// const hello = () => {
+//   setTimeout(() => {
+//     console.log('Hello Galaxy!')
+//   }, 1000)
+// }
 
-functions1(hello)
+// functions1(hello)
 
 // =======================≠=======================
 
- const buscarUsuario = (callback) => {
-  const erro = false
+//  const buscarUsuario = (callback) => {
+//   const erro = false
 
-  if (erro) callback('User finded', {nome: "Daniel", idade: 16})
-  else callback('User finded', {nome: "Daniel", idade: 12})
-}
-buscarUsuario((erro, usuario) => {
+//   if (erro) callback('User finded', {nome: "Daniel", idade: 16})
+//   else callback('User finded', {nome: "Daniel", idade: 12})
+// }
+// buscarUsuario((erro, usuario) => {
 
-const img = document.querySelector('.viewer')
+// const img = document.querySelector('.viewer')
 
-const testFunc = async () => {
-  const options = {
-    method: 'get',
-    headers: {
-      'Content-type': 'application/json',
-      'Authentication': 'Client-Id Q2tld8ltGnBRav2hRUdJihze4IOr05VYzf2vEHjAMy8'
-    }
-  }
-  try {
-    const resp = await fetch('https://jsonplaceholder.com/users')
-    const json = resp.json()
-    console.log(json)
-  }
-  catch(error) {
-    throw new Error('Seever Error')
-  }
-}
+// const testFunc = async () => {
+//   const options = {
+//     method: 'get',
+//     headers: {
+//       'Content-type': 'application/json',
+//       'Authentication': 'Client-Id Q2tld8ltGnBRav2hRUdJihze4IOr05VYzf2vEHjAMy8'
+//     }
+//   }
+//   try {
+//     const resp = await fetch('https://jsonplaceholder.com/users')
+//     const json = resp.json()
+//     console.log(json)
+//   }
+//   catch(error) {
+//     throw new Error('Seever Error')
+//   }
+// }
 
-testFunc()
+// testFunc()
 
 // ===================================================
 
-let age = 23
+// let age = 23
 
-if (age > 0 && age <= 14) {
-   console.log("You are a child!")
-} else if (age > 14 && age <= 17) {
-   console.log("You are an adolecent!")
-} else if (age > 17 && age >= 60) {
-   console.log("You are an adult!")
-} else if (age > 60) {
-   console.log("You are an old people!")
-}
+// if (age > 0 && age <= 14) {
+//    console.log("You are a child!")
+// } else if (age > 14 && age <= 17) {
+//    console.log("You are an adolecent!")
+// } else if (age > 17 && age >= 60) {
+//    console.log("You are an adult!")
+// } else if (age > 60) {
+//    console.log("You are an old people!")
+// }
+
+// ======================================================
+
+const users = [
+  {id: 1, name: 'João', idade: 23, profissao: 'Dentista'},
+  {id: 2, name: 'Miguel', idade: 14, profissao: 'Nenhuma'},
+  {id: 3, name: 'Felipe', idade: 41, profissao: 'Pediatra'},
+  {id: 4, name: 'Gabriel', idade: 35, profissao: 'Lixeiro'}
+]
+
+const onlyHigherThan18 = users.filter(users => users.idade > 18)
+console.log(onlyHigherThan18)
+
+const oneYearPassed = users.map(users => users.idade + 1)
+console.log(oneYearPassed)
+
+const onlyOneUser = users.find(users => users.id === 1)
+console.log(onlyOneUser)
